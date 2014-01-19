1ca385d412a56089d2c7f2c9b2b76ab93274af65
Joe Fleming <joe.fleming@gmail.com>
2014-01-18 18:31:41 -0700

    added todd's ping code, none of it running yet

--- hackPHX/Over_9000/sketch_jan18b/sketch_jan18b.ino
+++ hackPHX/Over_9000/sketch_jan18b/sketch_jan18b.ino
@@ -1,5 +1,5 @@
 #include <Wire.h>
-#include "FastLED.h"
+#include <FastLED.h>
 #include <ADXL345.h>
 #include <SeeedOLED.h>
 #include <math.h>
@@ -18,20 +18,31 @@ CRGB leds[NUM_LEDS];
 ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
 
 // Global variables
-int powerLevel;
-int count;
+int powerLevel; // power level; shown on the display, used for LEDs
+int count; // throttle the loop, mostly helpful for debugging
+
+int pingPin = 7; // pin for ping sensor (should use A5 on xadow)
+int ledPin = 2; // pin turn on LED (should use 1 on xadow)
+long duration; // duration for ping return with evaluates to distance
+long inches; // inches to object
+long feet; // feet to object
 
 void setup(){
+
   initSerial();
   initDisplay();
   FastLED.addLeds<NEOPIXEL, DATA_PIN, RGB>(leds, NUM_LEDS);
-  delay(2000);
+
+  // ping setup
+  pinMode(ledPin, OUTPUT);
+  // Serial.begin(9600);
+  digitalWrite(ledPin, LOW);    //make sure LED is off
 }
 
 void loop() {
   //Boring accelerometer stuff
   int x,y,z;
-  
+
   // Increment count and handle 3-axis debug output
   if (++count = 4) {
     if (DEBUG) {
@@ -42,41 +53,41 @@ void loop() {
       Serial.print(y);
       Serial.print(" , ");
       Serial.print(z);
-    
+
       // Print vector names
       refreshDataDisplay();
-      
+
       // Print vector values
       SeeedOled.setTextXY(1,5);           //Set the cursor to 0th Page, 0th Column
       SeeedOled.putNumber((int) x);
       SeeedOled.setTextXY(2,5);           //Set the cursor to 0th Page, 0th Column
       SeeedOled.putNumber((int) y);
       SeeedOled.setTextXY(3,5);           //Set the cursor to 0th Page, 0th Column
-      SeeedOled.putNumber((int) z);    
+      SeeedOled.putNumber((int) z);
     }
-    
+
     // Clear the readout
     SeeedOled.setTextXY(3,5);
     SeeedOled.putString("     ");
-    
+
     // Decrement powerLevel on idle
     if (powerLevel > 0) {
       powerLevel -= 250;
     }
-  
+
     // Reset the count
     count = 0;
   }
-  
+
   //read the accelerometer values and store them in variables  x,y,z
-  adxl.readXYZ(&x, &y, &z); 
+  adxl.readXYZ(&x, &y, &z);
 
   // Calculate new power level
   powerLevel += get_powerLevel();
-  
+
   // Print the power level
   print_powerLevel();
-  
+
   // Draw a power meter
   clearRow();
   draw_powerMeter(powerLevel);
@@ -85,31 +96,34 @@ void loop() {
   updateLED(powerLevel);
 
   count++;
+
+  pingLoop();
+
   delay(125);
 }
 
 void updateLED(int x) {
-    // Input: Number of LEDs to turn on
-    int i, j;
-    
-    x = x/1000;
-    
-    if (x <= 9) {
-      // Turn on correct number of LEDs
-      for (i=0; i<x; i++) {
-         leds[i] = CRGB::Red;
-      }
-      
-      // Turn off all remaining LEDs
-      for (j=NUM_LEDS-1; j>=x; j--) {
-         leds[j] = CRGB::Black;
-      }
-      
-      // Update the light setting
-      FastLED.show();
-    } else {
-        blinkAll();
+  // Input: Number of LEDs to turn on
+  int i, j;
+
+  x = x/1000;
+
+  if (x <= 9) {
+    // Turn on correct number of LEDs
+    for (i=0; i<x; i++) {
+       leds[i] = CRGB::Red;
     }
+
+    // Turn off all remaining LEDs
+    for (j=NUM_LEDS-1; j>=x; j--) {
+       leds[j] = CRGB::Black;
+    }
+
+    // Update the light setting
+    FastLED.show();
+  } else {
+      blinkAll();
+  }
 }
 
 void blinkAll(){
@@ -133,85 +147,85 @@ void clearRow() {
 }
 
 void draw_powerMeter(int x) {
-    int i, j;
-    char* meterString;
-    x = x/1000;
+  int i, j;
+  char* meterString;
+  x = x/1000;
 
-    SeeedOled.setTextXY(5,1);
-    SeeedOled.putString("[");
+  SeeedOled.setTextXY(5,1);
+  SeeedOled.putString("[");
 
-    switch (x) {
-      case 0:
-        meterString = "";
-        break;
-      case 1:
-        meterString = "#";
-        break;
-      case 2:
-        meterString = "##";
-        break;
-      case 3:
-        meterString = "###";
-        break;
-      case 4:
-        meterString = "####";
-        break;
-      case 5:
-        meterString = "#####";
-        break;
-      case 6:
-        meterString = "######";
-        break;
-      case 7:
-        meterString = "#######";
-        break;
-      case 8:
-        meterString = "########";
-        break;
-      case 9:
-        meterString = "#########";
-        break;
-      default:
-        meterString = "Over 9,000!";
-        break;
-    }
+  switch (x) {
+    case 0:
+      meterString = "";
+      break;
+    case 1:
+      meterString = "#";
+      break;
+    case 2:
+      meterString = "##";
+      break;
+    case 3:
+      meterString = "###";
+      break;
+    case 4:
+      meterString = "####";
+      break;
+    case 5:
+      meterString = "#####";
+      break;
+    case 6:
+      meterString = "######";
+      break;
+    case 7:
+      meterString = "#######";
+      break;
+    case 8:
+      meterString = "########";
+      break;
+    case 9:
+      meterString = "#########";
+      break;
+    default:
+      meterString = "Over 9,000!";
+      break;
+  }
 
-    SeeedOled.setTextXY(5,2);
-    SeeedOled.putString(meterString);
-    SeeedOled.setTextXY(5,13);
-    SeeedOled.putString("]");
+  SeeedOled.setTextXY(5,2);
+  SeeedOled.putString(meterString);
+  SeeedOled.setTextXY(5,13);
+  SeeedOled.putString("]");
 }
 
 void print_powerLevel() {
-    if (DEBUG) {
-      Serial.print("Power level :\n");
-      Serial.print(powerLevel);
-      Serial.print("\n");
-    }
-    
-    SeeedOled.setTextXY(1,0);
-    SeeedOled.putString("  Power Level  ");
-    SeeedOled.setTextXY(2,0);
-    SeeedOled.putString(" --------------");
-    SeeedOled.setTextXY(3,0);
-    SeeedOled.putString("|  > ");
-    SeeedOled.setTextXY(3,15);
-    SeeedOled.putString("|");
-    SeeedOled.setTextXY(3,5);
-    SeeedOled.putNumber(powerLevel);
-    SeeedOled.setTextXY(4,0);
-    SeeedOled.putString(" --------------");
+  if (DEBUG) {
+    Serial.print("Power level :\n");
+    Serial.print(powerLevel);
+    Serial.print("\n");
+  }
+
+  SeeedOled.setTextXY(1,0);
+  SeeedOled.putString("  Power Level  ");
+  SeeedOled.setTextXY(2,0);
+  SeeedOled.putString(" --------------");
+  SeeedOled.setTextXY(3,0);
+  SeeedOled.putString("|  > ");
+  SeeedOled.setTextXY(3,15);
+  SeeedOled.putString("|");
+  SeeedOled.setTextXY(3,5);
+  SeeedOled.putNumber(powerLevel);
+  SeeedOled.setTextXY(4,0);
+  SeeedOled.putString(" --------------");
 }
 
 int get_powerLevel() {
   int a,b,c; // starting accel
   int x,y,z; // final accel
   double diff_ax, diff_by, diff_cz; // difference variables
-  
+
   adxl.readXYZ(&x, &y, &z);
   delay(250);
   adxl.readXYZ(&a, &b, &c);
-  
+
   // Algorithm to calculate a pseudo-powerlevel
   diff_ax = abs(x) - abs(a);
   diff_by = abs(y) - abs(b);
@@ -291,4 +305,54 @@ void initSerial() {
   adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
   adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
   adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+}
+
+// ping sensor and distance measurement
+void pingLoop() {
+  ping();
+
+  //turn on led if objects are under 3 feet
+  if (feet < 3) {
+     digitalWrite(ledPin, HIGH);
+  }
+  else {
+     digitalWrite(ledPin, LOW);
+  };
+
+
+  if (inches < 24) {
+     Serial.print("inches=");
+  Serial.print(inches);
+  }
+  else {
+     Serial.print("feet=");
+  Serial.print(feet);
+  };
+
+  Serial.println();
+  // delay(10);
+}
+
+void ping() {
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // We give a short LOW pulse beforehand to ensure a clean HIGH pulse.
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  feet = inches / 12;
+}
+
+long microsecondsToInches(long microseconds) {
+  return microseconds / 74 / 2;
 }
